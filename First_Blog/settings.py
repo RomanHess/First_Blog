fbf5784a807af02c2003209092fe2520c7679c66
RomanHess <46488627+RomanHess@users.noreply.github.com>
2020-12-14 21:05:55 +0100

    Update settings.py

--- First_Blog/settings.py
+++ First_Blog/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!g*nki@_b6ff8hcjir*@+m=q_qlt=4wb$2j7&1y)tjb5(hzvit'
+SECRET_KEY = My_First_Key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
